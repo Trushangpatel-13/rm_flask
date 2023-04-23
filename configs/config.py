b036edf49219ef897ce3e3047b3f44321b01d7f7
Trushangpatel-13 <ptrushang@gmail.com>
2023-04-23 14:35:17 +0530

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,16 @@
+'''
 dbconfig = {
     "host":"localhost",
     "port":"3306",
     "username":"RM",
     "password":"Saurer_RM",
     "database":"saurer_rotor_motor"
+}
+'''
+dbconfig = {
+    "host":"containers-us-west-57.railway.app",
+    "port": 5719,
+    "username":"root",
+    "password":"NgcLLBBIIkG1eC4rmSbB",
+    "database":"railway"
 }
