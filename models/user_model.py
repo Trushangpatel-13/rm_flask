7cd99427087ee4732c062426a6a80caccdb1450b
Trushangpatel-13 <ptrushang@gmail.com>
2023-04-23 14:36:21 +0530

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -7,7 +7,7 @@ from configs.config import dbconfig
 
 class user_model():
     def __init__(self):
-        self.con = mysql.connector.connect(host=dbconfig['host'],user=dbconfig['username'],password=dbconfig['password'],database=dbconfig['database'])
+        self.con = mysql.connector.connect(host=dbconfig['host'],port=dbconfig['port'],user=dbconfig['username'],password=dbconfig['password'],database=dbconfig['database'])
         self.con.autocommit=True
         self.cur = self.con.cursor(dictionary=True)
         
